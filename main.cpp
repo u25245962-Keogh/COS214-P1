427145ef24a03af6b27969d38d95e259d711e3d1
u25085892-Bheki <u25085892@tuks.co.za>
2026-08-03 23:36:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,46 @@
 #include "TransformationRegistry.h"
+#include "CheckpointManager.h"
+#include "Pipeline.h"
+#include "Connector.h"
+#include "Transformation.h"
+#include "PostgresFactory.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(){
+
+TransformationRegistry* registry = new TransformationRegistry();
+
+
+DeduplicateStep* d = new DeduplicateStep();
+registry->registerStep("dedup", d );
+
+AggregateByRegionStep* a = new AggregateByRegionStep();
+registry->registerStep("aggregate", a );
+
+ConnectorFactory* cf = new PostgresFactory();
+
+BatchPipeline* bp = new BatchPipeline(cf);
+
+bp->addStep(registry->create("dedup"));
+bp->addStep(registry->create("aggregate"));
+
+CheckpointManager* cm = new CheckpointManager();
+bp->run();
+RunCheckpoint* cp = bp->createCheckpoint();
+cm->save(cp);
+
+delete registry;
+delete d;
+delete a;
+delete cf;
+delete bp;
+delete cm;
+
+    return 0;
+}#include "TransformationRegistry.h"
 #include "Pipeline.h"
 #include "Connector.h"
 #include "Transformation.h"
