ee05a5b1eafa8d9ac3d88552ae0f0b65a0b89ba0
Shreya <shreya.keogh@gmail.com>
2026-08-03 23:38:06 +0200

    hgvghv

--- main.cpp
+++ main.cpp
@@ -40,36 +40,4 @@ delete bp;
 delete cm;
 
     return 0;
-}#include "TransformationRegistry.h"
-#include "Pipeline.h"
-#include "Connector.h"
-#include "Transformation.h"
-#include "PostgresFactory.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iostream>
-
-int main(){
-
-TransformationRegistry* registry = new TransformationRegistry();
-
-
-DeduplicateStep* d = new DeduplicateStep();
-registry->registerStep("dedup", d );
-
-AggregateByRegionStep* a = new AggregateByRegionStep();
-registry->registerStep("aggregate", a );
-
-ConnectorFactory* cf = new PostgresFactory();
-
-BatchPipeline* bp = new BatchPipeline(cf);
-
-bp->addStep(registry->create("dedup"));
-bp->addStep(registry->create("aggregate"));
-
-
-
-
-    return 0;
 }
