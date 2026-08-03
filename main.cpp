012bbdcd2c31b24d494bbf6200e7cbb2ef653142
Shreya <shreya.keogh@gmail.com>
2026-08-03 23:06:50 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "TransformationRegistry.h"
+#include "Pipeline.h"
+#include "Connector.h"
+#include "Transformation.h"
+#include "PostgresFactory.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+int main(){
+
+TransformationRegistry registry = new TransformationRegistry();
+
+
+DeduplicateStep d = new DeduplicateStep();
+registry.registerStep("dedup", d );
+
+AggregateByRegionStep a = new AggregateByRegionStep();
+registry.registerStep("aggregate", a );
+
+
+
+
+
+    return 0;
+}
+
